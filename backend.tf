407a9541af03545ca096e407561f13d22944dd1b
Yanne <yannemanuela@gmail.com>
2025-03-31 21:46:11 -0400

    added my remote backend for terraform.tfstate formated

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-bans-terraform-state-bucket"
-    key            = "instance/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
+    bucket  = "my-bans-terraform-state-bucket"
+    key     = "instance/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
   }
 }
 # This backend configuration uses an S3 bucket to store the Terraform state file.
